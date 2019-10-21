450bf0b714791b1521c7a39f922b66614ddd7568
yenifersan <44785957+yenifersan@users.noreply.github.com>
2019-10-21 15:41:38 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio08/Ejercicio08/ViewModels/OperacionesViewModel.cs
@@ -0,0 +1,140 @@
+﻿
+using Ejercicio08.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Ejercicio08.ViewModels
+{
+    public class OperacionesViewModel : ViewModelBase
+    {
+
+        #region Propiedades
+        int valor1;
+        public int Valor1
+        {
+            get { return valor1; }
+            set
+            {
+                if (valor1 != value)
+                {
+                    valor1 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int valor2;
+        public int Valor2
+        {
+            get { return valor2; }
+            set
+            {
+                if (valor2 != value)
+                {
+                    valor2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int suma;
+        public int Suma
+        {
+            get { return suma; }
+            set
+            {
+                if (suma != value)
+                {
+                    suma = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resta;
+        public int Resta
+        {
+            get { return resta; }
+            set
+            {
+                if (resta != value)
+                {
+                    resta = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        int multiplica;
+        public int Multiplica
+        {
+            get { return multiplica; }
+            set
+            {
+                if (multiplica != value)
+                {
+                    multiplica = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        int divide;
+        public int Divide
+        {
+            get { return divide; }
+            set
+            {
+                if (divide != value)
+                {
+                    divide = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        #region Comandos 
+        public ICommand Sumar { protected set; get; }
+
+        public ICommand Restar { protected set; get; }
+
+        public ICommand Multiplicar { protected set; get; }
+
+        public ICommand Dividir { protected set; get; }
+        #endregion
+
+        #region Constructor
+
+        public OperacionesViewModel()
+        {
+
+            Sumar = new Command(() =>
+            {
+                Suma = Valor1 + Valor2;
+            });
+
+            Restar = new Command(() =>
+            {
+                Resta = Valor1 - Valor2;
+            });
+
+            Multiplicar = new Command(() =>
+            {
+                Multiplica = Valor1 * Valor2;
+            });
+
+            Dividir = new Command(() =>
+            {
+                Divide = Valor1 / Valor2;
+            });
+        }
+        #endregion
+
+    }
+}
---
+++ Ejercicio08/Ejercicio08/Views/Operaciones.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Ejercicio08.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio08.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operaciones : ContentPage
+    {
+        public Operaciones()
+        {
+            InitializeComponent();
+            this.BindingContext = new OperacionesViewModel();
+        }
+    }
+}
